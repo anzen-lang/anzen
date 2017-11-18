b598ecdbe5d5cf2910d82cef2cbb235ed883f7a1
Dimitri Racordon <dimitri.racordon@gmail.com>
2017-11-18 14:53:29 +0100

    Add LLVm as dependency

--- Package.swift
+++ Package.swift
@@ -5,16 +5,23 @@ import PackageDescription
 
 let package = Package(
     name: "anzen",
+    products: [
+        .executable(name: "anzenc", targets: ["anzenc"]),
+        .library(name: "AnzenLib", type: .static, targets: ["AnzenLib"]),
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/rxwei/Parsey.git", from: "2.0.0"),
         .package(url: "https://github.com/kyouko-taiga/SwiftProductGenerator.git", from: "1.0.1"),
+        .package(url: "https://github.com/trill-lang/LLVMSwift.git", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a
         // test suite. Targets can depend on other targets in this package, and on products in
         // packages which this package depends on.
         .target(name: "anzenc"  , dependencies: ["AnzenLib"]),
-        .target(name: "AnzenLib", dependencies: ["Parsey", "SwiftProductGenerator"]),
+        .target(name: "AnzenLib", dependencies: [
+            "Parsey", "SwiftProductGenerator", "LLVM",
+        ]),
     ]
 )
