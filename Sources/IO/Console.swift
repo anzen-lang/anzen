ef58f113c45029710e0287d15f8b8d4b38154245
Dimitri Racordon <dimitri.racordon@gmail.com>
2018-03-01 15:58:12 +0100

    Implement some IO utilities

---
+++ Sources/IO/Console.swift
@@ -0,0 +1,80 @@
+#if os(Linux)
+    import Glibc
+#else
+    import Darwin.C
+#endif
+
+public struct Console: TextOutputStream {
+
+    public init(ostream: UnsafeMutablePointer<FILE>) {
+        self.ostream = ostream
+    }
+
+    public struct Style: CustomStringConvertible, ExpressibleByArrayLiteral {
+
+        public init(_ attributes: Set<Int32>) {
+            self.attributes = attributes
+        }
+
+        public init(arrayLiteral: Style...) {
+            self.attributes = Set(arrayLiteral.reduce(Set<Int32>(), { $0.union($1.attributes) }))
+        }
+
+        public var description: String {
+            guard !self.attributes.isEmpty
+                else { return "" }
+            let attrs = self.attributes.map({ "\($0)" }).joined(separator: ";")
+            return "\u{001B}[\(attrs)m"
+        }
+
+        public static let reset         = Style([0])
+        public static let bold          = Style([1])
+        public static let italic        = Style([3])
+        public static let underline     = Style([4])
+        public static let strikethrough = Style([9])
+        public static let black         = Style([30])
+        public static let red           = Style([31])
+        public static let green         = Style([32])
+        public static let yellow        = Style([33])
+        public static let blue          = Style([34])
+        public static let magenta       = Style([35])
+        public static let cyan          = Style([36])
+        public static let white         = Style([37])
+        public static let `default`     = Style([38])
+
+        let attributes: Set<Int32>
+
+    }
+
+    public func write(_ string: String) {
+        fputs("\(string)", self.ostream)
+    }
+
+    public func print(
+        _ items: Any..., in style: Style = .default,
+        separator: String = " ", terminator: String = "\n")
+    {
+        let string = Console.isTerminalSupported
+            ? items.map({ "\(style)\($0)\(Style.reset)" })
+            : items.map({ "\($0)" })
+        self.write(string.joined(separator: separator) + terminator)
+    }
+
+    public static var out: Console {
+        return Console(ostream: stdout)
+    }
+
+    public static var err: Console {
+        return Console(ostream: stderr)
+    }
+
+    let ostream: UnsafeMutablePointer<FILE>
+
+    static var isTerminalSupported: Bool {
+        guard let capabilities = getenv("TERM")
+            else { return false }
+        return (String(cString: capabilities).lowercased() != "dumb")
+            && (isatty(fileno(stdout)) != 0)
+    }
+
+}
---
+++ Sources/IO/File.swift
@@ -0,0 +1,80 @@
+#if os(Linux)
+    import Glibc
+#else
+    import Darwin.C
+#endif
+
+public enum IOError: Error {
+
+    case fileNotFound(path: String) // ENOENT (2)
+    case permissionDenied           // EACCES (13)
+    case readOnly                   // EROFS  (30)
+    case other(errno: Int32)
+
+    public var number: Int32 {
+        switch self {
+        case .fileNotFound(_):      return ENOENT
+        case .permissionDenied:     return EACCES
+        case .readOnly:             return EROFS
+        case .other(errno: let no): return no
+        }
+    }
+
+    public static func == (lhs: IOError, rhs: Int32) -> Bool {
+        return lhs.number == rhs
+    }
+
+}
+
+public class File {
+
+    public enum Mode: String {
+        case read      = "r"
+        case write     = "w"
+        case readWrite = "rw"
+    }
+
+    public init(path: String, mode: Mode = .read) throws {
+        self.openMode = mode
+        self.fp = fopen(path, mode.rawValue)
+        if self.fp == nil {
+            switch errno {
+            case ENOENT: throw IOError.fileNotFound(path: path)
+            case EACCES: throw IOError.permissionDenied
+            case EROFS : throw IOError.readOnly
+            default    : throw IOError.other(errno: errno)
+            }
+        }
+    }
+
+    deinit {
+        if self.fp != nil {
+            fclose(self.fp)
+        }
+    }
+
+    public func read() -> String {
+        guard self.size > 0 else { return "" }
+
+        let buffer = [CChar](repeating: 0, count: self.size + 1)
+        fread(UnsafeMutablePointer(mutating: buffer), MemoryLayout<CChar>.size, size, fp)
+        return String(cString: buffer)
+    }
+
+    @discardableResult
+    public func write(data: String) -> Bool {
+        let cString = Array(data.utf8)
+        return fwrite(cString, cString.count, 1, self.fp) == 1
+    }
+
+    public lazy var size: Int = {
+        fseek(self.fp, 0, SEEK_END)
+        defer { rewind(self.fp) }
+        return ftell(self.fp)
+    }()
+
+    public let openMode: Mode
+
+    private let fp: UnsafeMutablePointer<FILE>?
+
+}
