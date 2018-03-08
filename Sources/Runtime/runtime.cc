17ba08805124a5b44bab8577fae7bbab16196de8
Dimitri Racordon <dimitri.racordon@gmail.com>
2018-03-08 09:25:19 +0100

    Bug fix

--- Sources/Runtime/runtime.cc
+++ Sources/Runtime/runtime.cc
@@ -7,7 +7,7 @@ void azn_gc_object_init(azn_gc_object_t* object, size_t size, void (*destructor)
     object->value = malloc(size);
     object->destructor = destructor;
     object->count = (size_t*)malloc(sizeof(size_t));
-    azn_gc_object_retain(object);
+    *(object->count) = 1;
 }
 
 void azn_gc_object_retain(azn_gc_object_t* object) {
