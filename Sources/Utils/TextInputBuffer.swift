9ad33f3e8593c0461ec6bd72e5bd8a5ed8201e94
Dimitri Racordon <dimitri.racordon@gmail.com>
2019-07-12 15:51:22 +0200

    Improve documentation

--- Sources/Utils/TextInputBuffer.swift
+++ Sources/Utils/TextInputBuffer.swift
@@ -1,17 +1,25 @@
 import SystemKit
 
+/// A type that can be the target of text reading operations.
+///
+/// A text input buffer is an object that has methods to read one or several characters as strings
+/// from some buffer of characters.
 public protocol TextInputBuffer {
 
+  /// Reads `count` characters starting from `offet`.
   func read(count: Int, from offset: Int) throws -> String
+
+  /// Reads all characters of the buffer.
   func read() throws -> String
 
 }
 
 extension TextInputBuffer {
 
-  public func read(lines: Int) throws -> [String] {
+  /// Reads up to `count` lines of the buffer.
+  public func read(lines count: Int) throws -> [String] {
     return try read().split(separator: "\n", omittingEmptySubsequences: false)
-      .prefix(lines)
+      .prefix(count)
       .map({ String($0) })
   }
 
