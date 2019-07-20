5566878129366ea53fbc2a810d22bd6ec5fafd3b
Dimitri Racordon <dimitri.racordon@gmail.com>
2019-07-21 00:27:19 +0200

    Implement the parser for nullref

--- Sources/Parser/Parser+Expression.swift
+++ Sources/Parser/Parser+Expression.swift
@@ -96,6 +96,10 @@ extension Parser {
     var errors: [ParseError] = []
 
     switch token.kind {
+    case .nullref:
+      consume()
+      expression = NullRef(module: module, range: token.range)
+
     case .integer:
       consume()
       expression = Literal(value: Int(token.value!)!, module: module, range: token.range)
